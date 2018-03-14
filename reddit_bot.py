be8595975d21ee703662c85674a660ff1de185da
Carson Clark <wackerboo@hotmail.com>
2018-03-13 22:22:40 -0700

    Update reddit_bot.py

--- reddit_bot.py
+++ reddit_bot.py
@@ -14,9 +14,9 @@ def bot_login():
 def run_bot(r):
 	print "obtaining 25 comments"
 	for comment in r.subreddit('indianpeoplefacebook').comments(limit=25):
-		if "real" in comment.body:
-			print "string with \"real\" found!" + comment.id
-			comment.reply("Is anything real?")
+		if "milktruk" in comment.body:
+			print "string with \"milktruk\" found!" + comment.id
+			comment.reply("This meme stems from: ?")
 			print "replied to comment" + comment.id
 
 r = bot_login()
